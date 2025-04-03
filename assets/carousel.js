56cce741ca7ba1ebb26e86a4aefd72ad36cf081c
Jolyn Tran <tranj193@newschool.edu>
2025-04-03 03:54:39 -0400

    buttons

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,25 +1,25 @@
-// const carousel = document.querySelector('.carousel');
-// let isScrolling;
+const carousel = document.querySelector('.carousel');
+let isScrolling;
 
-// carousel.addEventListener('scroll', () => {
-//   window.clearTimeout(isScrolling);
-//   isScrolling = setTimeout(() => {
-//     const cards = document.querySelectorAll('.noise-card');
-//     const middle = window.innerWidth / 2;
-//     let closest = null;
-//     let closestDist = Infinity;
+carousel.addEventListener('scroll', () => {
+  window.clearTimeout(isScrolling);
+  isScrolling = setTimeout(() => {
+    const cards = document.querySelectorAll('.noise-card');
+    const middle = window.innerWidth / 2;
+    let closest = null;
+    let closestDist = Infinity;
 
-//     cards.forEach(card => {
-//       const box = card.getBoundingClientRect();
-//       const center = box.left + box.width / 2;
-//       const dist = Math.abs(middle - center);
-//       if (dist < closestDist) {
-//         closestDist = dist;
-//         closest = card;
-//       }
-//     });
+    cards.forEach(card => {
+      const box = card.getBoundingClientRect();
+      const center = box.left + box.width / 2;
+      const dist = Math.abs(middle - center);
+      if (dist < closestDist) {
+        closestDist = dist;
+        closest = card;
+      }
+    });
 
-//     cards.forEach(c => c.classList.remove('active'));
-//     if (closest) closest.classList.add('active');
-//   }, 150);
-// });
+    cards.forEach(c => c.classList.remove('active'));
+    if (closest) closest.classList.add('active');
+  }, 150);
+});
