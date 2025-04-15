95df03c0be78ab8ddb3253aead56ad625745a3fe
Jolyn Tran <tranj193@newschool.edu>
2025-04-15 03:29:29 -0400

    Update modal.js

--- assets/modal.js
+++ assets/modal.js
@@ -1,42 +1,42 @@
 // Help button
-let button = document.querySelector('#help')
-let modal = document.querySelector('#dialog') 
-let closeButton = modal.querySelector('.close') 
+let button = document.querySelector("#help");
+let modal = document.querySelector("#dialog");
+let closeButton = modal.querySelector(".close");
 
-button.onclick = () => { 
-	modal.showModal() 
-}
+button.onclick = () => {
+  modal.showModal();
+};
 
 closeButton.onclick = () => {
-	modal.close() 
-}
+  modal.close();
+};
 
-modal.onclick = (event) => { 
-	if (event.target == modal) { 
-		modal.close() 
-	}
-}
+modal.onclick = (event) => {
+  if (event.target == modal) {
+    modal.close();
+  }
+};
 
 // Preset drawer toggle
 window.addEventListener("DOMContentLoaded", () => {
-	const presetsDrawer = document.getElementById("presets-drawer");
-	const sleepDrawer = document.getElementById("sleep-timer-drawer");
-	const presetsButton = document.getElementById("presets");
+  const presetsDrawer = document.getElementById("presets-drawer");
+  const sleepDrawer = document.getElementById("sleep-timer-drawer");
+  const presetsButton = document.getElementById("presets");
 
-	presetsButton.addEventListener("click", () => {
-		presetsDrawer.classList.toggle("visible");
-		sleepDrawer.classList.remove("visible"); // hide the other one
-	});
+  presetsButton.addEventListener("click", () => {
+    presetsDrawer.classList.toggle("visible");
+    sleepDrawer.classList.remove("visible"); // hide the other one
+  });
 });
 
 // Sleep timer drawer toggle
 window.addEventListener("DOMContentLoaded", () => {
-	const sleepDrawer = document.getElementById("sleep-timer-drawer");
-	const presetsDrawer = document.getElementById("presets-drawer");
-	const sleepButton = document.getElementById("sleep");
+  const sleepDrawer = document.getElementById("sleep-timer-drawer");
+  const presetsDrawer = document.getElementById("presets-drawer");
+  const sleepButton = document.getElementById("sleep");
 
-	sleepButton.addEventListener("click", () => {
-		sleepDrawer.classList.toggle("visible");
-		presetsDrawer.classList.remove("visible"); // hide the other one
-	});
+  sleepButton.addEventListener("click", () => {
+    sleepDrawer.classList.toggle("visible");
+    presetsDrawer.classList.remove("visible"); // hide the other one
+  });
 });
