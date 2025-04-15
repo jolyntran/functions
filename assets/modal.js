b52913d0936078470e18a40637ab9e83c581fe48
Jolyn Tran <tranj193@newschool.edu>
2025-04-15 03:24:07 -0400

    toggling the drawers

--- assets/modal.js
+++ assets/modal.js
@@ -17,16 +17,26 @@ modal.onclick = (event) => {
 	}
 }
 
-// Preset button
+// Preset drawer toggle
 window.addEventListener("DOMContentLoaded", () => {
-	const drawer = document.querySelector(".top-drawer");
+	const presetsDrawer = document.getElementById("presets-drawer");
+	const sleepDrawer = document.getElementById("sleep-timer-drawer");
 	const presetsButton = document.getElementById("presets");
 
-	if (drawer && presetsButton) {
-		presetsButton.addEventListener("click", () => {
-			drawer.classList.toggle("visible");
-		});
-	}
+	presetsButton.addEventListener("click", () => {
+		presetsDrawer.classList.toggle("visible");
+		sleepDrawer.classList.remove("visible"); // hide the other one
+	});
 });
 
-// Sleep timer 
+// Sleep timer drawer toggle
+window.addEventListener("DOMContentLoaded", () => {
+	const sleepDrawer = document.getElementById("sleep-timer-drawer");
+	const presetsDrawer = document.getElementById("presets-drawer");
+	const sleepButton = document.getElementById("sleep");
+
+	sleepButton.addEventListener("click", () => {
+		sleepDrawer.classList.toggle("visible");
+		presetsDrawer.classList.remove("visible"); // hide the other one
+	});
+});
